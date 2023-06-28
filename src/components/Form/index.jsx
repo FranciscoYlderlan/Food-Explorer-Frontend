c94a05b864b45e8f2bbbe1bc7bca40d07446f10f
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-06-28 10:44:57 -0300

    feat: Added Form component

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from './styles'
+
+export function Form({ children }) {
+  return <Container>{children}</Container>
+}
---
+++ src/components/Form/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Container = styled.form`
+  display: flex;
+  gap: 3.2rem;
+`
