0b7d348ec956a30c98864f91e05a5bb19c981064
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-06-30 11:24:27 -0300

    feat: Added fonts on theme

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom/client'
 import { theme } from './styles/theme.js'
 import GlobalStyle from './styles/global.js'
 import { ThemeProvider } from 'styled-components'
-import { Table } from './components/Table/index.jsx'
+import { SignIn } from './pages/SignIn/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Table />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from './styles'
+
+export function SignIn() {
+  return <Container></Container>
+}
