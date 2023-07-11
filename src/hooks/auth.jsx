7c2c095e4167d7f18cf2a933e7ad41d97251b18a
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-07-11 20:22:34 -0300

    feat: Added jwt_decode

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,6 +1,6 @@
-import { createContext, useContext } from 'react'
+import { useState, createContext, useContext } from 'react'
 import { api } from '../services/api'
-
+import jwt_decode from 'jwt-decode'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { toastConfig } from '../services/toast'
@@ -8,13 +8,27 @@ import { toastConfig } from '../services/toast'
 const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+  const [data, setData] = useState({})
   async function signIn({ email, password }) {
     try {
-      await toast.promise(api.post('/sessions', { email, password }), {
-        pending: 'Por favor aguarde...',
-        error: 'Não foi possível efetuar login.',
-        ...toastConfig,
-      })
+      const response = await toast.promise(
+        api.post('/sessions', { email, password }),
+        {
+          pending: 'Por favor aguarde...',
+          error: 'Não foi possível efetuar login.',
+          ...toastConfig,
+        },
+      )
+      const { user, token } = response.data
+
+      api.defaults.headers.common.Authorization = `Bearer ${token}`
+
+      const decodedToken = await jwt_decode(token)
+
+      const isAdmin = decodedToken ? decodedToken.isAdmin : null
+      console.log(isAdmin)
+
+      setData({ user, token })
     } catch (error) {
       if (error.response) {
         toast.error(error.response.data.description)
@@ -23,7 +37,7 @@ function AuthProvider({ children }) {
   }
 
   return (
-    <AuthContext.Provider value={{ signIn }}>
+    <AuthContext.Provider value={{ signIn, user: data.user }}>
       <ToastContainer
         position="top-right"
         autoClose={5000}
