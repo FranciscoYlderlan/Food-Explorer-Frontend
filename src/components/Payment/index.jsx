82982646c85fa7568b9b4de3c33fa62699e5b1fc
FranciscoYlderlan <ylderlan@nca.ufma.br>
2023-07-21 17:37:03 -0300

    feat: Added checkout images

--- src/components/Payment/index.jsx
+++ src/components/Payment/index.jsx
@@ -5,24 +5,40 @@ import { Button } from '../Button'
 import { MdOutlinePix } from 'react-icons/md'
 import { CreditCard } from '@phosphor-icons/react'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import qrCodeExample from '../../assets/qr-code-example.svg'
+import stroke from '../../assets/Stroke.svg'
+import checkCircle from '../../assets/CheckCircle.svg'
 
-export function Payment() {
+const status = {
+  OPENED: 0,
+  SEND: 1,
+  APPROVED: 3,
+}
+
+export function Payment({ ...rest }) {
   const [isCreditOpen, setIsCreditOpen] = useState(false)
   const [isPixOpen, setIsPixOpen] = useState(true)
-
+  const [statusCheckout, setStatusCheckout] = useState(0)
+  const [image, setImage] = useState(qrCodeExample)
   function handleCreditClick() {
     setIsCreditOpen(true)
     setIsPixOpen(false)
+    setStatusCheckout((prevState) => prevState + 1)
   }
 
   function handlePixClick() {
     setIsPixOpen(true)
     setIsCreditOpen(false)
   }
-
+  useEffect(() => {
+    if (statusCheckout === status.SEND) {
+      setImage(stroke)
+    } else if (statusCheckout === status.APPROVED) {
+      setImage(checkCircle)
+    }
+  }, [statusCheckout])
   return (
     <Container>
       <Pix isOpen={isPixOpen} onClick={handlePixClick}>
@@ -35,7 +51,7 @@ export function Payment() {
       </Credit>
       <Content>
         {isPixOpen ? (
-          <img src={qrCodeExample} alt="QR code" />
+          <img src={image} alt="QR code" />
         ) : (
           <Form>
             <Input placeholder="0000 0000 0000 0000" />
